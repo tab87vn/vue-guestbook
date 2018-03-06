46f228e00907c610b1cdba21bb1f5b18ed78e5a7
Tuan-Anh BUI <tuananh.bui@euranova.eu>
2018-03-06 23:33:15 +0100

    Correct guestbook form

--- frontend/src/components/Guestbook.vue
+++ frontend/src/components/Guestbook.vue
@@ -2,8 +2,14 @@
   <v-container fluid>
     <v-form v-model="valid" ref="form" lazy-validation>
     <v-text-field
-      label="Name"
-      v-model="name"
+      label="First name"
+      v-model="firstName"
+      :rules="nameRules"
+      :counter="10"
+      required />
+    <v-text-field
+      label="Last name"
+      v-model="lastName"
       :rules="nameRules"
       :counter="10"
       required />
@@ -13,21 +19,20 @@
       :rules="emailRules"
       required />
     <v-select
-      label="Item"
-      v-model="select"
-      :items="items"
-      :rules="[v => !!v || 'Item is required']"
+      label="Gender"
+      v-model="gender"
+      :items="genders"
+      :rules="[v => !!v || 'Gender is required']"
       required></v-select>
     <v-checkbox
-      label="Do you agree?"
+      label="Are you sure you want to do this?"
       v-model="checkbox"
       :rules="[v => !!v || 'You must agree to continue!']"
       required />
 
     <v-btn
       @click="submit"
-      :disabled="!valid"
-    >
+      :disabled="!valid">
       submit
     </v-btn>
     <v-btn @click="clear">clear</v-btn>
@@ -36,10 +41,14 @@
 </template>
 
 <script>
-  export default {
-    name: 'guestbook',
+import axios from 'axios'
+
+export default {
+  name: 'guestbook',
+  data: () => ({
     valid: true,
-    name: '',
+    firstName: '',
+    lastName: '',
     nameRules: [
       v => !!v || 'Name is required',
       v => (v && v.length <= 10) || 'Name must be less than 10 characters'
@@ -49,15 +58,32 @@
       v => !!v || 'E-mail is required',
       v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
     ],
-    select: null,
-    items: [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4'
+    gender: null,
+    genders: [
+      'Male',
+      'Female',
+      'Gay',
+      'Lesbian',
+      'Other'
     ],
     checkbox: false
+  }),
+  methods: {
+    submit: function () {
+      if (this.$refs.form.validate()) {
+        axios.post('/api/submit', {
+          name: this.firstName + ' ' + this.lastName,
+          email: this.email,
+          gender: this.gender,
+          checkbox: this.checkbox
+        })
+      }
+    },
+    clear: function () {
+      this.$refs.form.reset()
+    }
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
